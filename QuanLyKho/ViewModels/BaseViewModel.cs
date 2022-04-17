5577dad8e0a09740480e6372ee2225033ed98040
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2022-04-17 14:25:29 +0700

    implement MVVM architecture in project

---
+++ QuanLyKho/ViewModels/BaseViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if(execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            try
+            {
+                return _canExecute == null ? true : _canExecute((T)parameter);
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+    }
+}
---
+++ QuanLyKho/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKho.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vào trong MainViewModel -> DataContext của MainWindow.xaml");
+        }
+    }
+}
